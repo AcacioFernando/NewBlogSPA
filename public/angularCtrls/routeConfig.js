bb7a1c08c273161fd3e2467544d7fcf227ff27af
Acacio <afo.fernando@gmail.com>
2016-03-26 12:18:51 -0300

    Ajustes

--- public/angularCtrls/routeConfig.js
+++ public/angularCtrls/routeConfig.js
@@ -45,18 +45,3 @@ angular.module("myApp").config(function ($routeProvider, $locationProvider) {
     }
 });
 
-/*
- angular.module("myApp").run([
- '$rootScope',
- function($rootScope) {
- // see what's going on when the route tries to change
- $rootScope.$on('$routeUpdate', function(event, next, current) {
- // next is an object that is the route that we are starting to go to
- // current is an object that is the route where we are currently
- var currentPath = current.originalPath;
- var nextPath = next.originalPath;
-
- console.log('Starting to leave %s to go to %s', currentPath, nextPath);
- });
- }
- ]);*/
