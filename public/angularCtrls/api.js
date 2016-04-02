2930fad92642a0623884403315ac22b20060a25b
Acacio <afo.fernando@gmail.com>
2016-04-02 14:33:02 -0300

    Ajustes

--- public/angularCtrls/api.js
+++ public/angularCtrls/api.js
@@ -1,2 +1,29 @@
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ["ngRoute", "ngSanitize", "ngCkeditor","ngFileUpload"]);
+angular.module('myApp', ["ngRoute", "ngSanitize", "ngCkeditor","ngFileUpload"])
+    .run(['$rootScope', function($rootScope) {
+        var hadRouteChange = false;
+        $rootScope.$on("$routeChangeStart", function() {
+            hadRouteChange = true;
+        });
+        function hookAngularBoomerang() {
+            if (window.BOOMR && BOOMR.version) {
+                if (BOOMR.plugins && BOOMR.plugins.Angular) {
+                    BOOMR.plugins.Angular.hook($rootScope, hadRouteChange);
+                }
+                return true;
+            }
+        }
+        if (!hookAngularBoomerang()) {
+            if (document.addEventListener) {
+                document.addEventListener("onBoomerangLoaded", hookAngularBoomerang);
+            }
+            else if (document.attachEvent) {
+                document.attachEvent("onpropertychange", function(e) {
+                    e = e || window.event;
+                    if (e && e.propertyName === "onBoomerangLoaded") {
+                        hookAngularBoomerang();
+                    }
+                });
+            }
+        }
+    }]);
