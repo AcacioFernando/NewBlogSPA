334e14fcc9691542087be40f25e8370975196cae
Acacio Oliveira <afo.fernando@gmail.com>
2016-04-02 18:01:19 -0300

    Ajustes

--- public/angularCtrls/api.js
+++ public/angularCtrls/api.js
@@ -2,9 +2,11 @@
 angular.module('myApp', ["ngRoute", "ngSanitize", "ngCkeditor","ngFileUpload"])
     .run(['$rootScope', function($rootScope) {
         var hadRouteChange = false;
+
         $rootScope.$on("$routeChangeStart", function() {
             hadRouteChange = true;
         });
+
         function hookAngularBoomerang() {
             if (window.BOOMR && BOOMR.version) {
                 if (BOOMR.plugins && BOOMR.plugins.Angular) {
@@ -13,11 +15,11 @@ angular.module('myApp', ["ngRoute", "ngSanitize", "ngCkeditor","ngFileUpload"])
                 return true;
             }
         }
+
         if (!hookAngularBoomerang()) {
             if (document.addEventListener) {
                 document.addEventListener("onBoomerangLoaded", hookAngularBoomerang);
-            }
-            else if (document.attachEvent) {
+            } else if (document.attachEvent) {
                 document.attachEvent("onpropertychange", function(e) {
                     e = e || window.event;
                     if (e && e.propertyName === "onBoomerangLoaded") {
