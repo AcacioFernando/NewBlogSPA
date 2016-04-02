b80319b594c6473d7a1aadc65aca7f23f93f098b
Acacio <afo.fernando@gmail.com>
2016-04-02 18:06:07 -0300

    boomerang

--- public/angularCtrls/api.js
+++ public/angularCtrls/api.js
@@ -2,10 +2,12 @@
 angular.module('myApp', ["ngRoute", "ngSanitize", "ngCkeditor","ngFileUpload"])
     .run(['$rootScope', function($rootScope) {
         var hadRouteChange = false;
+
         $rootScope.$on("$routeChangeStart", function() {
             hadRouteChange = true;
         });
-        function hook() {
+
+        function hookAngularBoomerang() {
             if (window.BOOMR && BOOMR.version) {
                 if (BOOMR.plugins && BOOMR.plugins.Angular) {
                     BOOMR.plugins.Angular.hook($rootScope, hadRouteChange);
@@ -14,14 +16,14 @@ angular.module('myApp', ["ngRoute", "ngSanitize", "ngCkeditor","ngFileUpload"])
             }
         }
 
-        if (!hook()) {
+        if (!hookAngularBoomerang()) {
             if (document.addEventListener) {
-                document.addEventListener("onBoomerangLoaded", hook);
+                document.addEventListener("onBoomerangLoaded", hookAngularBoomerang);
             } else if (document.attachEvent) {
                 document.attachEvent("onpropertychange", function(e) {
                     e = e || window.event;
                     if (e && e.propertyName === "onBoomerangLoaded") {
-                        hook();
+                        hookAngularBoomerang();
                     }
                 });
             }
